ecb2f4449aff9cc8428d58f99f008c84c71002b8
SaSaChu <teresa7397@gmail.com>
2022-06-14 12:52:41 +0800

    靜態頁over，rotary3 tab要調整

--- js/public.js
+++ js/public.js
@@ -1,3 +1,8 @@
 $(function() {
+
+	var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+	var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
   
 });
